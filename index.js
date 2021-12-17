4187a8796b62eac7f15f59a33f7d8826a4d1f26d
bipana <bipana.shrestha@zegal.com>
2021-12-17 20:56:59 +0545

    changed port and db name

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const authController = require("./auth/authController");
 app.use("/api/v1", authController);
 
 mongoose
-  .connect("mongodb://localhost:27017/datahere", {
+  .connect("mongodb://localhost:27017/shop247", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
